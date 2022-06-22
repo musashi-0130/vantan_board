23200017ac6b68c571c325e145171a6bdeb38bfb
musashi-0130 <71209492+musashi-0130@users.noreply.github.com>
2022-06-22 14:48:07 +0900

    update php

--- regster.php
+++ regster.php
@@ -1,3 +1,43 @@
+<?php
+ $message = '';
+ try {
+     $DBSERVER = 'localhost';
+     $DBUSER = 'board';
+     $DBPASSWD = 'boardpw';
+     $DBNAME = 'board';
+ 
+     $dsn = 'mysql:'
+         . 'host=' . $DBSERVER . ';'
+         . 'dbname=' . $DBNAME . ';'
+         . 'charset=utf8';
+     $pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
+ } catch (Exception $e) {
+     $message = "接続に失敗しました: {$e->getMessage()}";
+ }
+ 
+ // 入力が全て入っていたらユーザーを作成する
+ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password'])) {
+     $name = $_POST['name'];
+     $mail = $_POST['mail'];
+     $password = $_POST['password'];
+ 
+     $sql = 'INSERT INTO `users` (name, mail, password, created, modified)';
+     $sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
+     $stmt = $pdo->prepare($sql);
+     $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
+     $stmt->bindValue(':mail', $mail, \PDO::PARAM_STR);
+     $stmt->bindValue(':password', $password, \PDO::PARAM_STR);
+     $result = $stmt->execute();
+     if($result) {
+         $message = 'ユーザーを作成しました';
+     } else {
+       $message = '登録に失敗しました';
+     }
+ }
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
